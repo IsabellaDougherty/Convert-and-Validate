6e429a2e64f41f52b249ea891a0166824d93ff0e
IsabellaDougherty <belladougherty@isu.edu>
2023-02-08 19:31:24 -0700

    Committing

--- ConvertAndValidate.vb
+++ ConvertAndValidate.vb
@@ -7,8 +7,40 @@
 
 Imports System
 
-Module Program
-    Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+Module ConvertAndValidate
+    Sub Main()
+
+        'for testing the function add this to your Sub Main()
+
+        Dim aValidNumber As Integer
+        Dim usernResponse As String
+
+        Do
+            Console.WriteLine($"Enter a number:")
+            usernResponse = Console.ReadLine()
+            If ConversionValid(usernResponse, aValidNumber) = True Then
+                Console.WriteLine($"{usernResponse} converted successfully to {aValidNumber}!")
+            Else
+                Console.WriteLine($"Oops, {usernResponse} does not seem to be a number")
+            End If
+        Loop
+
     End Sub
+
+    'make this work
+
+
+    Function ConversionValid(convertThisString, toThisInteger) As Boolean
+
+        Dim status As Boolean
+
+        Try
+
+        Catch ex As Exception
+
+        End Try
+
+        Return status
+
+    End Function
 End Module
