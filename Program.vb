7c51be8acbf0e5afc29b0fb0ec2037ec6bddece4
IsabellaDougherty <belladougherty@isu.edu>
2023-02-08 19:25:13 -0700

    Add project files.

---
+++ Program.vb
@@ -0,0 +1,14 @@
+'Isabella Dougherty
+'RCET0265
+'Spring 2023
+'
+
+
+
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Console.WriteLine("Hello World!")
+    End Sub
+End Module
